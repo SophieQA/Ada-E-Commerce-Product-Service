55cc5ab3a7555ea857a82216a28bf3bac2553993
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2026-04-12 14:47:07 -0400

    introduces sevice layer and implmenents SQS consumer polling

---
+++ app/consumers/consumer.py
@@ -0,0 +1,50 @@
+import os
+import json
+import boto3
+from dotenv import load_dotenv
+from ..utilities import decrement_stock
+
+load_dotenv()
+
+
+def process_message(message):
+    sns_envelope = json.loads(message.body)
+    event = json.loads(sns_envelope["Message"])
+
+    if event["event_type"] == "order.placed":
+        order = event["payload"]
+
+        # process each item within THIS order
+        for item in order["items"]:
+            decrement_stock(item["product_id"], item["quantity"])
+
+    return True
+
+
+def poll(wait_time=20):
+    while True:
+        sqs = boto3.resource('sqs')
+        queue = sqs.Queue(os.environ["QUEUE_URL"])
+
+        print("Polling For Messages...")
+
+        messages = queue.receive_messages(
+            MaxNumberOfMessages=10,
+            VisibilityTimeout=30,
+            WaitTimeSeconds=wait_time
+        )
+
+        for message in messages:
+            # try:
+                print(f"Processing Message: {message.message_id}")
+                is_processed = process_message(message)
+
+                if is_processed:
+                    message.delete()
+                    print(f"Message {message.message_id} PROCESSED!")
+                    continue
+            # except Exception as e:
+                print(f"Message {message.message_id} NOT PROCESSED!")
+
+if __name__ == "__main__":
+  poll(wait_time=20)
--- app/routes/products_routes.py
+++ app/routes/products_routes.py
@@ -2,8 +2,8 @@ import os
 from uuid import uuid4
 from decimal import Decimal
 from ..tables.products import products_table
-from flask import Blueprint, request, Response
-from .route_utilities import build_and_run_update, get_items_with_filters, validate_item
+from flask import Blueprint, request, Response, jsonify
+from ..utilities import build_and_run_update, get_items_with_filters, validate_item
 
 bp = Blueprint("products_bp", __name__, url_prefix="/products")
 KEY_NAME = os.environ.get("KEY_NAME")
@@ -24,20 +24,38 @@ def create_product():
 
 @bp.get("/")
 def get_all_products():
-    items = get_items_with_filters(products_table, request.args)
-    return items
+    try:
+        items = get_items_with_filters(products_table, request.args)
+        return jsonify(items), 200
+    except ValueError as e:
+        return {"error": str(e)}, 400
+    except RuntimeError as e:
+        return {"error": str(e)}, 500
 
 
 @bp.get("/<product_id>")
 def get_single_product(product_id):
-    product = validate_item(products_table, KEY_NAME, product_id)
-    return product
+    try:
+        product = validate_item(products_table, product_id)
+        return product
+    except LookupError as e:
+        return {"message": e.args[0]}, 404
+    except RuntimeError as e:
+        return {"error": str(e)}, 500
 
 
 @bp.patch("/<product_id>")
 def update_product(product_id):
     body = request.get_json()
-    return build_and_run_update(products_table, {KEY_NAME: product_id}, body)
+    try:
+        build_and_run_update(products_table, product_id, body)
+        return Response(status=204, mimetype="application/json")
+    except LookupError as e:
+        return {"message": e.args[0]}, 404
+    except ValueError as e:
+        return {"message": str(e)}, 400
+    except RuntimeError as e:
+        return {"error": str(e)}, 500
 
 
 @bp.delete("/<product_id>")
