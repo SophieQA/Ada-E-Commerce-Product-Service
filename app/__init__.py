6f790ad88d1366c64ab0280c8d7be78a8b14cf5b
Mikelle <mmikelle.wade@adadevelopersacademy.org>
2026-04-24 13:49:14 -0400

    implements CORS

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes.products_routes import bp as products_bp
 
 
 def create_app():
     app = Flask(__name__)
+    CORS(app)
 
     app.register_blueprint(products_bp)
 
