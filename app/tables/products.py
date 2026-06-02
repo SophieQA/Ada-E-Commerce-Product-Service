f267f3704bbfa5d15adf06b16c245eeefc965775
apradoada <adrian.prado@adadevelopersacademy.org>
2026-06-01 19:33:54 -0500

    Update Table and Utilities to use updated botocore configuration

--- app/tables/products.py
+++ app/tables/products.py
@@ -3,6 +3,5 @@ import boto3
 from dotenv import load_dotenv
 
 load_dotenv()
-
-dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+dynamodb = boto3.resource('dynamodb', region_name=os.environ.get("REGION_NAME"))
 products_table = dynamodb.Table(name=os.environ.get("TABLE_NAME"))
